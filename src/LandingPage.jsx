a14359c9899ddd43d29ff7e30c1bffb45caccea8
Lexi <mlexifarris@gmail.com>
2023-12-26 21:06:37 -0700

    changed landing page image sizing & options

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -1,12 +1,12 @@
 export function LandingPage() {
   return (
     <div
-      className="relative overflow-hidden bg-cover bg-no-repeat"
+      className="relative overflow-hidden bg-cover bg-no-repeat bg-blue-500 opacity-25 "
       style={{
-        backgroundPosition: "50%",
+        backgroundPosition: "100%",
         backgroundImage:
           "url('https://images.unsplash.com/photo-1593358578769-b7a7cf27cba7?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
-        height: "300px",
+        height: "900px",
       }}
     ></div>
   );
