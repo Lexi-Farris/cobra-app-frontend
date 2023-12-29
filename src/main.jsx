2699e5db2b741081da64f3ed6d99d53b47189700
Lexi <mlexifarris@gmail.com>
2023-12-28 18:33:44 -0700

    fixed header

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { Carousel, initTE } from "tw-elements";
 initTE({ Carousel }, true); // set second parameter to true if you want to use a debugger
+import "tw-elements-react/dist/css/tw-elements-react.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,css,jsx,tsx}", "./node_modules/tw-elements/dist/js/**/*.js"],
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", "./node_modules/tw-elements-react/dist/js/**/*.js"],
   theme: {
     colors: {
       "custom-blue": {
@@ -19,8 +19,37 @@ export default {
         100: "#FFB703",
         200: "#FB8500",
       },
+      extend: {},
     },
   },
-  plugins: [require("tw-elements/dist/plugin.cjs")],
   darkMode: "class",
+  plugins: [require("tw-elements-react/dist/plugin.cjs")],
 };
+
+// OG CONTENT HERE
+//** @type {import('tailwindcss').Config} */
+// export default {
+//   content: ["./index.html", "./src/**/*.{js,ts,css,jsx,tsx}", "./node_modules/tw-elements/dist/js/**/*.js"],
+//   theme: {
+//     colors: {
+//       "custom-blue": {
+//         100: "#8ecae6",
+//         200: "#219EBC",
+//         300: "#126782",
+//         400: "#023047",
+//         500: "#023047",
+//       },
+//       "light-white": {
+//         100: "#f1faee",
+//         200: "#fefae0",
+//         300: "#f8f9fa",
+//       },
+//       "interesting-orange": {
+//         100: "#FFB703",
+//         200: "#FB8500",
+//       },
+//     },
+//   },
+//   plugins: [require("tw-elements/dist/plugin.cjs")],
+//   darkMode: "class",
+// };
